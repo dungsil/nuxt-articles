65fd4a3d155e7f9590fa9c2e8acc3df2ef0c83dd
김용건 (Kim Younggeon) <mail@kyg.kr>
2022-01-06 19:54:19 +0900

    feat: add `builder:watch` hook

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import log from 'consola'
+import { readFile, stat } from 'fs/promises'
 import { defineNuxtModule, isNuxt2 } from '@nuxt/kit'
 import { basename, join } from 'pathe'
-import { readFile, stat } from 'fs/promises'
-import type { Article, ArticleListData, Nuxt3ContentOptions } from './types'
+import type { Nuxt } from '@nuxt/schema'
 
 import { name } from '../package.json'
 import readArticlePathList from './read-articles'
 import markdownToHtml from './markdown-to-html'
 import createApi from './create-api'
+import type { Article, ArticleListData, Nuxt3ContentOptions } from './types'
 
 // Should include types only
 export * from './types'
@@ -29,38 +30,38 @@ export default defineNuxtModule<Nuxt3ContentOptions>({
       return
     }
 
-    // Create articles before nuxt build
-    nuxt.hook('build:before', async () => {
-      // read articles directory
-      const articlePathList = await readArticlePathList(join(nuxt.options.srcDir, options.articlesDir))
-
-      const articlesList: ArticleListData[] = [] // list api
-      const articleDetails: Record<string, Article> = {} // content api
-
-      for (const articlePath of articlePathList) {
-        const filename = basename(articlePath).replace(/\.md$/, '')
-        const { birthtime: published } = await stat(articlePath)
-        const articleContent = (await readFile(articlePath)).toString()
-        const vFile = await markdownToHtml(articleContent)
-
-        // add articles list
-        articlesList.push({
-          ...vFile.data,
-          slug: filename,
-          published: published.toISOString(),
-        })
-
-        // add article details
-        articleDetails[filename] = {
-          ...vFile.data,
-          published: published.toISOString(),
-          content: String(vFile),
-        }
-      }
-
-      await createApi(join(nuxt.options.srcDir, 'server/api/'), options.apiPath, articlesList, articleDetails)
-
-      log.success('Create articles API')
-    })
+    nuxt.hook('build:before', async () => buildArticles(options, nuxt))
+    nuxt.hook('builder:watch', async () => buildArticles(options, nuxt))
   },
 })
+
+async function buildArticles(options: Nuxt3ContentOptions, nuxt: Nuxt): Promise<void> {
+  // read articles directory
+  const articlePathList = await readArticlePathList(join(nuxt.options.srcDir, options.articlesDir))
+
+  const articlesList: ArticleListData[] = [] // list api
+  const articleDetails: Record<string, Article> = {} // content api
+
+  for (const articlePath of articlePathList) {
+    const filename = basename(articlePath).replace(/\.md$/, '')
+    const { birthtime: published } = await stat(articlePath)
+    const articleContent = (await readFile(articlePath)).toString()
+    const vFile = await markdownToHtml(articleContent)
+
+    // add articles list
+    articlesList.push({
+      ...vFile.data,
+      slug: filename,
+      published: published.toISOString(),
+    })
+
+    // add article details
+    articleDetails[filename] = {
+      ...vFile.data,
+      published: published.toISOString(),
+      content: String(vFile),
+    }
+  }
+
+  await createApi(join(nuxt.options.srcDir, 'server/api/'), options.apiPath, articlesList, articleDetails)
+}
